0f9a664993675c8b0bf7c6866134e30269c36e7b
Leonardo Di Matteo <leonardo.di.matteo@cern.ch>
2015-05-21 17:53:33 +0200

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R source code to perform squared matrix inversion with caching
+## CPU consuming operations are minimized (i.e. matrix inversion)
+## Information from first matrix inversion is cached and reused
+## NB: Package assumes input is square invertible matrix
 
-## Write a short comment describing this function
-
+## Function built to associate to input matrix additional info, in
+## particular value of inverse matrix (by defaul NULL)
 makeCacheMatrix <- function(x = matrix()) {
-
+    invM <- NULL
+    set <- function(y) {
+        x <<- y
+        invM <<- NULL
+    }
+    get <- function() x
+    setInvM <- function(invMatrix) invM <<- invMatrix
+    getInvM <- function() invM
+    list(set = set, get = get,
+         setInvM = setInvM,
+         getInvM = getInvM)
 }
 
 
-## Write a short comment describing this function
-
+## Smart matrix inverter. Inversion computation happens only
+## if not already computed and stored in memory
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    tempInv <- x$getInvM()
+    ## Check if inverse matrix already in memory, if yes return it
+    if(!is.null(tempInv)) {
+        message("getting cached data")
+        return(tempInv)
+    }
+    dataM <- x$get()
+    ## If inverse is NULL, compute it with R built in function
+    tempInv <- solve(dataM)
+    x$setInvM(tempInv)
+    return(tempInv)
 }
